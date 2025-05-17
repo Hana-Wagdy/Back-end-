ee2ffe3ed1135f573456ff8d801701d047d65357
Hana Wagdy <161964697+Hana-Wagdy@users.noreply.github.com>
2025-05-17 22:17:51 +0300

    reads the freebooks from DB

--- books/models.py
+++ books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    category = models.CharField(max_length=50, blank=True)
+    available = models.BooleanField(default=True)
+    cover = models.ImageField(upload_to='photos/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
--- books/views.py
+++ books/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-
 def freebooks(request):
-  return render(request, 'freeBooks.html')
+    books = Book.objects.all()
+    return render(request, 'freeBooks.html', {'books': books})
 
 def bookdetails(request):
     return render(request, 'bookdetailed.html')
